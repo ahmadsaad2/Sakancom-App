35f1e2c44daff458f490fd2d2fff8fe3323c1b46
shahadjawabreh <shahadjawabreh3@gmail.com>
2023-08-08 00:46:21 +0300

    inputscanner

--- src/main/java/owner/util/Tenant.java
+++ src/main/java/owner/util/Tenant.java
@@ -275,8 +275,6 @@ public class Tenant {
 
   }
   public void advertiseFurniture() {
-    inputscanner = new Scanner(System.in);
-
     logger.info("Enter furniture details:");
 
     logger.info(NAME_LABEL);
