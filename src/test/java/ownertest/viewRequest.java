6c995a3f4a231eb07f22812661ec16e10fc111e8
shahadjawabreh <shahadjawabreh3@gmail.com>
2023-08-07 09:33:50 +0300

    com

--- src/test/java/ownertest/viewRequest.java
+++ src/test/java/ownertest/viewRequest.java
@@ -1,9 +1,7 @@
 package ownertest;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
 
-import java.util.List;
 
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
